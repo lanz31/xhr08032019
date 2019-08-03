14028e3b55cc543f7060440716412966c17a4dc1
Lanz <lorenz53192@gmail.com>
2019-08-03 14:16:15 +0800

    Save changes.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var generate = document.getElementById('generate');
 
 generate.addEventListener('click', function () {
     users.innerHTML = "";
-    
+
     var no = document.getElementById('no').value;
     var url = `https://randomuser.me/api/?results=${no}`;
 
@@ -13,21 +13,16 @@ generate.addEventListener('click', function () {
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
             var data = JSON.parse(xhr.response).results;
-        
+
             for (var x = 0; x < data.length; x++) {
-                //var li = document.createElement("li");
-                var p = document.createElement("p");
+                var paragraph = document.createElement("p");
+                paragraph.innerText = `${x}: ${data[x].name.title} ${data[x].name.first} ${data[x].name.last}`;
+                paragraph.id = "p" + x;
+                document.querySelector('#users').appendChild(paragraph);
+
                 var imahe = document.createElement("img");
-
                 imahe.src = data[x].picture.thumbnail;
-
-                p.innerText = `
-                    ${x+1}
-                    ${data[x].name.title} ${data[x].name.first} ${data[x].name.last}
-                `;
-                
-                users.appendChild(imahe);
-                users.appendChild(p);
+                document.querySelector("#" + paragraph.id).append(imahe);
             }
         }
     };
